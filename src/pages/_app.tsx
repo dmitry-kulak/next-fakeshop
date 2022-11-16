15879dbd5c27d3dc0841eccd9c3892f43e590915
dmitry-kulak <dmitry.kulak11@gmail.com>
2022-11-16 23:56:42 +0300

    next-fakeshop-5: add Montserrat default font

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,11 +1,24 @@
 import { type AppType } from "next/app";
+import localFont from "@next/font/local";
 
 import { trpc } from "../utils/trpc";
 
 import "../styles/globals.css";
 
+// doesn't work via @next/font/google import at the moment
+const montserrat = localFont({ src: "./Montserrat-VariableFont_wght.ttf" });
+
 const MyApp: AppType = ({ Component, pageProps }) => {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <style jsx global>{`
+        html {
+          font-family: ${montserrat.style.fontFamily};
+        }
+      `}</style>
+      <Component {...pageProps} />
+    </>
+  );
 };
 
 export default trpc.withTRPC(MyApp);
