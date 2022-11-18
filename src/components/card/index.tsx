98379e77d35fbe3f6a888f76c593c224880d3a51
Алексей Скурлатов <Scarlatum@yandex.ru>
2022-11-18 23:09:08 +0800

    next-fakeshop-1: Apply mock data and bindings to `Product Card` component.

--- src/components/card/index.tsx
+++ src/components/card/index.tsx
@@ -15,22 +15,26 @@ import { Label } from '../label';
 // Variants
 import { ContainerVariant } from '../label/types/variants'
 
+// TW Font Bindings
+import Montserrat from '../../fonts/montserrat'
+
 const ImageParams: Partial<StaticImageData> = {
   height  : 600,
   width   : 300,
 } as const;
 
-interface ProductCardProps extends Product {
-  test ?: number
+export interface ProductCardProps extends Product {
+  productLink : string
 }
 
 export const ProductCard: FC<ProductCardProps> = (props) => {
 
   const Tags = [ props.category ].map(cat => <Tag name={ cat } key={ cat } />)
     
+  // TODO: #8 [ Fix target ]: Need to fix tailwind color schema @Scarlatum  
   return <div className='bg-[#FFFFFF] flex flex-col rounded-xl gap-y-2 p-4 w-[360px] h-fit'>
 
-    <div className='rounded-lg p-4 border-4 border-gray-900 overflow-hidden'>
+    <div className={`${ Montserrat.variable  } font-sans rounded-lg p-4 border-4 border-gray-900 overflow-hidden`}>
       <Image 
         { ...ImageParams } 
         src={ props.image } 
@@ -51,14 +55,16 @@ export const ProductCard: FC<ProductCardProps> = (props) => {
 
     <div className='flex flex-col gap-y-2'>
       <p className='text-sm'>{ props.description }</p>
-      <a href={ './' } className="text-sm mx-auto underline">
-        Читать подробнее в разделе товара
-      </a>
+      <Link href={ props.productLink }>
+        <a className="text-sm mx-auto underline">
+          Читать подробнее в разделе товара
+        </a>
+      </Link>
     </div>
 
     <Separator />
 
-    <Label variant={ ContainerVariant.Price } params={{ cost: Number(props.price), sign: '$' }}/>
+    <Label variant={ ContainerVariant.Price } params={{ cost: props.price.toNumber(), sign: '$' }}/>
 
     <div className='flex flex-col gap-y-3 gap-x-1'>
       <Button />
