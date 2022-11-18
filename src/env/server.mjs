6abf1b3f153e182a0dbb9a0f8801641241470dcd
dmitry-kulak <dmitry.kulak11@gmail.com>
2022-11-18 19:22:11 +0300

    Feature: prettier and editor config

--- src/env/server.mjs
+++ src/env/server.mjs
@@ -3,16 +3,13 @@
  * This file is included in `/next.config.mjs` which ensures the app isn't built with invalid env vars.
  * It has to be a `.mjs`-file to be imported there.
  */
-import {serverSchema} from "./schema.mjs";
-import {env as clientEnv, formatErrors} from "./client.mjs";
+import { serverSchema } from "./schema.mjs";
+import { env as clientEnv, formatErrors } from "./client.mjs";
 
 const _serverEnv = serverSchema.safeParse(process.env);
 
 if (!_serverEnv.success) {
-  console.error(
-    "❌ Invalid environment variables:\n",
-    ...formatErrors(_serverEnv.error.format()),
-  );
+  console.error("❌ Invalid environment variables:\n", ...formatErrors(_serverEnv.error.format()));
   throw new Error("Invalid environment variables");
 }
 
