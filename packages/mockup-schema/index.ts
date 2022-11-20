c3d684049d47e832b6ac060f2a96b8f3b8c5a2e9
Алексей Скурлатов <Scarlatum@yandex.ru>
2022-11-20 23:30:11 +0800

    [ Refacatoring ]: Now used namespaces instead a class syntax.

--- packages/mockup-schema/index.ts
+++ packages/mockup-schema/index.ts
@@ -1,11 +1,3 @@
-import path from 'path';
-import runner, { type IRunProps } from './source/main';
+import { MockupSchemaParser, defaults } from './source/main.js';
 
-const FILE_NAME = 'generated-variables.txt';
-
-const defaultRunnerProps: IRunProps = {
-  dist              : path.resolve(__dirname, './schema'),
-  relativeFilePath  : path.resolve(__dirname, `./assets/${ FILE_NAME }`)
-}
-
-runner.run(defaultRunnerProps);
+MockupSchemaParser.run(defaults.defaultRunnerProps);
