31e9132ea2bdf7365ccdcb7227d40abb9f740446
Алексей Скурлатов <Scarlatum@yandex.ru>
2022-12-11 06:04:55 +0400

    TODO
    Хоть это и не сильно в приоритетах, так как пока это более чем проработка разметки, но потом желательно mock данные для этого завести.
    По крайней мере, сделать рендер ссылок более декларативным.

--- src/components/Layout/Footer.tsx
+++ src/components/Layout/Footer.tsx
@@ -10,6 +10,8 @@ const Footer = () => {
       <div className="flex flex-col justify-between md:flex-row">
         <Container className="m-0 w-full text-center shadow md:w-1/4">Placeholder</Container>
 
+        // TODO: Хоть это и не сильно в приоритетах, так как пока это более чем проработка разметки, но потом желательно mock данные для этого завести.
+        // По крайней мере, сделать рендер ссылок более декларативным. 
         <div className="mt-4 grid grid-cols-2 justify-items-center md:flex md:flex-wrap md:justify-between md:space-x-5">
           <LinksGroup label="Главный раздел">
             <LinksGroup.Link className={linksClassname} href="/">
